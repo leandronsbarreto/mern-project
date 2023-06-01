d9d24d8c5ec2b638f12184e35fad2a8d83728d41
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-01 23:12:21 +0200

    Update App.js

--- mern-app/src/App.js
+++ mern-app/src/App.js
@@ -1,15 +1,29 @@
-import './App.css';
-import home from './pages/home';
-import donate from './pages/donate';
-import getInvolved from './pages/get_involved';
-import login from './pages/login';
-import newsPress from './pages/news_press';
+import Home from './pages/home';
+import Donate from './pages/donate';
+import GetInvolved from './pages/get_involved';
+import Login from './pages/login';
+import NewsPress from './pages/news_press';
+import NotFoundPage from './pages/not_found';
+import styleApp from './styles/styleApp';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div style={styleApp.App}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/donate' element={<Donate />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/getinvolved' element={<GetInvolved />} />
+          <Route path='/articles' element={<NewsPress />} />
+          <Route path='/articles/:articleId' element={<NewsPress />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
