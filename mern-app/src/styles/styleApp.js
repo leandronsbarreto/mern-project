b7edc477407ed6c5eb83352917051834c766eae5
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-01 23:12:02 +0200

    Create styleApp.js

---
+++ mern-app/src/styles/styleApp.js
@@ -0,0 +1,15 @@
+const styleApp = {
+  App: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    textAlign: 'left',
+    backgroundColor: 'aqua',
+    margin: '0',
+    width: '100%',
+    minHeight: '100vh'
+  }
+};
+
+export default styleApp;
