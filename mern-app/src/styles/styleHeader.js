96b0588fedc59be65aa278133d53249032846186
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-02 06:48:27 +0200

    Create styleHeader.js

---
+++ mern-app/src/styles/styleHeader.js
@@ -0,0 +1,14 @@
+const styleHeader = {
+  //<div style={styleHeader.containerHeader}>
+  containerHeader: {
+    color: 'var(--dark-green)',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: '15vh',
+    width: '100%'
+  }
+};
+
+export default styleHeader;
