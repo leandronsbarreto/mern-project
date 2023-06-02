247ceedbab994167ab3d23dfec2b29c8e545a03d
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-02 06:48:34 +0200

    Create styleContent.js

---
+++ mern-app/src/styles/styleContent.js
@@ -0,0 +1,14 @@
+const styleContent = {
+  //<div style={styleContent.containerContent}>
+  containerContent: {
+    color: 'var(--dark-green)',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'column',
+    height: '70vh',
+    width: '100%'
+  }
+};
+
+export default styleContent;
