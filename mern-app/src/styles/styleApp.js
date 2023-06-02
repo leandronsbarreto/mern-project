2add3f7356982a5052e48e2581c783d6cabbe137
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-02 06:48:37 +0200

    Update styleApp.js

--- mern-app/src/styles/styleApp.js
+++ mern-app/src/styles/styleApp.js
@@ -5,7 +5,6 @@ const styleApp = {
     alignItems: 'center',
     flexDirection: 'column',
     textAlign: 'left',
-    backgroundColor: 'aqua',
     margin: '0',
     width: '100%',
     minHeight: '100vh'
