2135ff2c13d4a29a37851e444bc44ac5376edca0
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-02 06:48:39 +0200

    Update not_found.js

--- mern-app/src/pages/not_found.js
+++ mern-app/src/pages/not_found.js
@@ -1,5 +1,20 @@
+import styleNotFound from '../styles/stylenot_found';
+
 const NotFoundPage = () => {
-  return <h1>This is NotFoundPage</h1>;
+  return (
+    <>
+      <div style={styleNotFound.containerNotFound}>
+        <h1>Ops! Page not found.</h1>
+        <div style={styleNotFound.containerImgNotFound}>
+          <img
+            style={styleNotFound.imgNotFound}
+            src={process.env.PUBLIC_URL + '/images/pagenotfound.png'}
+            alt='Illustration with the error code 404 which means Page not found.'
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFoundPage;
