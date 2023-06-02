77d7a2e342982000ced24eaa779f652bb4916867
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-02 06:48:56 +0200

    Create Content.js

---
+++ mern-app/src/components/Content.js
@@ -0,0 +1,11 @@
+import styleContent from '../styles/styleContent';
+
+const Content = () => {
+  return (
+    <>
+      <main style={styleContent.containerContent}></main>
+    </>
+  );
+};
+
+export default Content;
