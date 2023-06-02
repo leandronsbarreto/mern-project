276627285e37f2b607497f94eb8a8c4b1562c005
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-02 06:47:50 +0200

    Create stylenot_found.js

---
+++ mern-app/src/styles/stylenot_found.js
@@ -0,0 +1,30 @@
+const styleNotFound = {
+  //<div style={styleNotFound.containerNotFound}>
+  containerNotFound: {
+    color: 'var(--dark-green)',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'column',
+    minHeight: 'auto',
+    width: '100%'
+  },
+
+  //<div style={styleNotFound.containerImgNotFound}>
+  containerImgNotFound: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    minHeight: 'auto',
+    width: '80%'
+  },
+
+  //<img style={styleNotFound.imgNotFound}>
+  imgNotFound: {
+    minHeight: 'auto',
+    width: '80%'
+  }
+};
+
+export default styleNotFound;
