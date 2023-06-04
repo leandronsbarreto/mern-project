932b337587c4b90873756bce9da696c3d9fdcd2b
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-04 14:49:00 +0200

    Update styleHeader.js

--- mern-app/src/styles/styleHeader.js
+++ mern-app/src/styles/styleHeader.js
@@ -1,13 +1,45 @@
 const styleHeader = {
   //<div style={styleHeader.containerHeader}>
   containerHeader: {
-    color: 'var(--dark-green)',
+    backgroundColor: 'var(--gray)',
     display: 'flex',
-    justifyContent: 'space-evenly',
+    justifyContent: 'center',
     alignItems: 'center',
-    flexDirection: 'row',
     height: '15vh',
     width: '100%'
+  },
+
+  insideContainerHeader: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
+    width: '95%'
+  },
+
+  containerImgHeader: {
+    backgroundColor: 'var(--light-green)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '95%',
+    width: '48%',
+    borderRadius: '0.938rem'
+  },
+
+  imgHeader: {
+    height: 'auto',
+    width: '60%'
+  },
+
+  buttonHeader: {
+    backgroundColor: 'var(--light-green)',
+    height: '40%',
+    width: '20%',
+    border: 'none',
+    fontSize: 'clamp(0.5rem, 0.4652rem + 1.1142vw, 1rem)',
+    fontWeight: '500',
+    borderRadius: '0.313rem'
   }
 };
 
