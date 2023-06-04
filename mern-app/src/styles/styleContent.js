b0407e2ca0493152b1b7d8550539f52ef51dedab
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-04 14:49:05 +0200

    Update styleContent.js

--- mern-app/src/styles/styleContent.js
+++ mern-app/src/styles/styleContent.js
@@ -1,95 +1,132 @@
 const styleContent = {
   //<div style={styleContent.containerContent}>
+  //Very small, mobile, and tablets
+  //Portrait
   containerContent: {
-    color: 'var(--black)',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    gap: '1rem',
     flexDirection: 'column',
-    height: '70vh',
+    height: '75vh',
     width: '100%'
   },
 
-  headerOne: {
-    backgroundColor: 'var(--light-green)',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    height: '10vh',
-    width: '95%',
-    padding: '0.5rem',
-    borderTopLeftRadius: '0.938rem',
-    borderTopRightRadius: '0.938rem',
-    fontSize: 'clamp(0.875rem, 0.8216rem + 1.7994vw, 1.125rem)'
-  },
-
+  //<div style={styleContent.containerCoverImg}>
+  //Very small, mobile, and tablets
+  //Portrait
   containerCoverImg: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
-    height: '25vh',
+    height: '35vh',
     width: '95%'
   },
 
-  coverImg: {
+  //<img style={styleContent.coverImgVerySmall}/>
+  //Very small
+  //Portrait
+  coverImgVerySmall: {
     height: 'auto',
-    width: '90%'
+    width: '95%'
   },
 
-  headerTwo: {
-    backgroundColor: 'var(--gray)',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
+  //<img style={styleContent.coverImgMobile}/>
+  //Mobile
+  //Portrait
+  coverImgMobile: {
     height: 'auto',
-    width: '95%',
-    padding: '0.5rem',
-    fontSize: 'clamp(0.5rem, 0.4466rem + 1.7994vw, 0.75rem)',
-    textAlign: 'justify'
+    width: 'clamp(15.97375rem, 12.15rem + 21.4667vw, 18.59rem)'
   },
 
+  //<img style={styleContent.coverImgTablet}/>
+  //Tablet
+  //Portrait
+  coverImgTablet: {
+    height: 'auto',
+    width: 'clamp(18.59rem, 3.8929rem + 48.8885vw, 27.359375rem)'
+  },
+
+  //<div style={styleContent.menuBox}>
+  //Very small, mobile, and tablets
+  //Portrait
   menuBox: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column',
-    height: '25vh',
+    height: '34vh',
     width: '95%'
   },
 
+  //<div style={styleContent.menuSubBoxTop}>
+  //Very small, mobile, and tablets
+  //Portrait
   menuSubBoxTop: {
     display: 'flex',
     justifyContent: 'space-between',
-    alignItems: 'start',
     flexDirection: 'row',
     height: '50%',
     width: '100%'
   },
 
-  insideSubBoxTop: {
+  //<div style={styleContent.insideSubBoxTopLeft}>
+  //Very small, mobile, and tablets
+  //Portrait
+  insideSubBoxTopLeft: {
     backgroundColor: 'var(--light-green)',
-    height: '90%',
-    width: '47%'
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: '95%',
+    width: '48%',
+    borderTopLeftRadius: '0.938rem',
+    fontSize: 'clamp(0.5rem, 0.4652rem + 1.1142vw, 1rem)',
+    fontWeight: '500'
   },
 
-  insideSubBoxBottomLeft: {
-    backgroundColor: 'var(--light-green)',
-    height: '90%',
-    width: '47%',
-    borderBottomLeftRadius: '0.938rem'
+  //<div style={styleContent.miniBlock}>
+  //Very small, mobile, and tablets
+  //Portrait
+  miniBlock: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '50%',
+    width: '85%'
   },
 
-  insideSubBoxBottomRight: {
-    backgroundColor: 'var(--light-green)',
-    height: '90%',
-    width: '47%',
-    borderBottomRightRadius: '0.938rem'
+  //<div style={styleContent.containerImgMiniBlock}>
+  //Very small, mobile, and tablets
+  //Portrait
+  containerImgMiniBlock: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: '100%',
+    width: '100%'
   },
 
+  //<div style={styleContent.insideSubBoxTopRight}>
+  //Very small, mobile, and tablets
+  //Portrait
+  insideSubBoxTopRight: {
+    backgroundColor: 'var(--light-green)',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: '95%',
+    width: '48%',
+    borderTopRightRadius: '0.938rem',
+    fontSize: 'clamp(0.5rem, 0.4652rem + 1.1142vw, 1rem)',
+    fontWeight: '500'
+  },
+
+  //<div style={styleContent.menuSubBoxTop}>
+  //Very small, mobile, and tablets
+  //Portrait
   menuSubBoxBottom: {
     display: 'flex',
     justifyContent: 'space-between',
@@ -97,6 +134,57 @@ const styleContent = {
     flexDirection: 'row',
     height: '50%',
     width: '100%'
+  },
+
+  //<div style={styleContent.insideSubBoxBottomLeft}>
+  //Very small, mobile, and tablets
+  //Portrait
+  insideSubBoxBottomLeft: {
+    backgroundColor: 'var(--light-green)',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: '95%',
+    width: '48%',
+    borderBottomLeftRadius: '0.938rem',
+    fontSize: 'clamp(0.5rem, 0.4652rem + 1.1142vw, 1rem)',
+    fontWeight: '500'
+  },
+
+  //<div style={styleContent.insideSubBoxBottomRight}>
+  //Very small, mobile, and tablets
+  //Portrait
+  insideSubBoxBottomRight: {
+    backgroundColor: 'var(--light-green)',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: '95%',
+    width: '48%',
+    borderBottomRightRadius: '0.938rem',
+    fontSize: 'clamp(0.5rem, 0.4652rem + 1.1142vw, 1rem)',
+    fontWeight: '500'
+  },
+
+  //<img style={styleContent.imgMiniBlock}
+  //Very small, mobile, and tablets
+  //Portrait
+  imgMiniBlock: {
+    height: 'auto',
+    width: '17.5%'
+  },
+
+  //<div style={styleContent.arrow}>
+  //Very small, mobile, and tablets
+  //Portrait
+  arrow: {
+    backgroundColor: 'var(--gray)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '1.875rem',
+    width: '1.875rem',
+    borderRadius: '50%'
   }
 };
 
