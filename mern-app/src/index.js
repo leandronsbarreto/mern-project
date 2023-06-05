070f656c0e0c8827d70e060cb546adeffa0eb108
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-06 01:04:17 +0200

    Update index.js

--- mern-app/src/index.js
+++ mern-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './teste.css';
+import './slick.css';
+import './slick-theme.css';
 
 import App from './App';
 
