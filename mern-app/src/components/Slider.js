7978f6fc179084726fb3630a1ea8eb4918a9a455
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-06 21:02:20 +0200

    Update Slider.js

--- mern-app/src/components/Slider.js
+++ mern-app/src/components/Slider.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import Slider from 'react-slick';
+import dataCarousel from '../data/dataCarouselHome.json';
 
 const Carousel = () => {
   const settings = {
@@ -17,46 +18,40 @@ const Carousel = () => {
     swipeToSlide: true
   };
   return (
-    <div className='slider'>
+    <>
       <Slider {...settings}>
-        <div className='containerPhoto'>
-          <img
-            className='photo'
-            src={process.env.PUBLIC_URL + '/images/tom-peyre-costa.jpg'}
-          />
-        </div>
-        <div className='containerPhoto'>
-          <img
-            className='photo'
-            src={process.env.PUBLIC_URL + '/images/julie-ricard-small.jpg'}
-          />
-        </div>
-        <div className='containerPhoto'>
-          <img
-            className='photo'
-            src={process.env.PUBLIC_URL + '/images/tom-peyre-costa.jpg'}
-          />
-        </div>
-        <div className='containerPhoto'>
-          <img
-            className='photo'
-            src={process.env.PUBLIC_URL + '/images/julie-ricard-small.jpg'}
-          />
-        </div>
-        <div className='containerPhoto'>
-          <img
-            className='photo'
-            src={process.env.PUBLIC_URL + '/images/tom-peyre-costa.jpg'}
-          />
-        </div>
-        <div className='containerPhoto'>
-          <img
-            className='photo'
-            src={process.env.PUBLIC_URL + '/images/julie-ricard-small.jpg'}
-          />
-        </div>
+        <img
+          className='photo'
+          src={process.env.PUBLIC_URL + '/images/julie-ricard-small.jpg'}
+          alt={dataCarousel.img1.alt}
+          title={dataCarousel.img1.author}
+        />
+        <img
+          className='photo'
+          src={process.env.PUBLIC_URL + '/images/ev-small.jpg'}
+          alt={dataCarousel.img2.alt}
+          title={dataCarousel.img2.author}
+        />
+        <img
+          className='photo'
+          src={process.env.PUBLIC_URL + '/images/moises-gonzalez-small.jpg'}
+          alt={dataCarousel.img3.alt}
+          title={dataCarousel.img3.author}
+        />
+        <img
+          className='photo'
+          src={process.env.PUBLIC_URL + '/images/jonathan-ramalho-small.jpg'}
+          alt={dataCarousel.img4.alt}
+          title={dataCarousel.img4.author}
+        />
+        <img
+          className='photo'
+          src={process.env.PUBLIC_URL + '/images/kevin-buckert-small.jpg'}
+          alt={dataCarousel.img5.alt}
+          title={dataCarousel.img5.author}
+        />
       </Slider>
-    </div>
+    </>
   );
 };
 
