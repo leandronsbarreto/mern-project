785341383f4daf2b5e69e467f284cf84f86e5356
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-10 00:40:30 +0200

    Update styleFooter.js

--- mern-app/src/styles/styleFooter.js
+++ mern-app/src/styles/styleFooter.js
@@ -1,6 +1,31 @@
 const styleFooter = {
   //<div style={styleFooter.containerFooter}>
-  //Very small, mobile, tablet
+  //Very small
+  //Portrait
+  containerFooterVerySmall: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    height: '30vh',
+    width: '100%'
+  },
+
+  //<div style={styleFooter.containerFooterLand}>
+  //Mobile and tablets
+  //Landscape
+  containerFooterLand: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: '30vh',
+    width: '100%'
+  },
+
+  //<div style={styleFooter.containerFooter}>
+  //Mobile and tablets
+  //Portrait
   containerFooter: {
     display: 'flex',
     justifyContent: 'center',
@@ -11,7 +36,8 @@ const styleFooter = {
   },
 
   //<div style={styleFooter.footerPart}>
-  //Very small, mobile, tablet
+  //Very small, mobile and tablets
+  //Portrait
   footerPart: {
     backgroundColor: 'var(--white)',
     color: 'var(--black)',
@@ -22,8 +48,22 @@ const styleFooter = {
     width: '70%'
   },
 
+  //<div style={styleFooter.containerElementsfooterPartLand}>
+  //Mobile and tablets
+  //Landscape
+  containerElementsfooterPartLand: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: '0.3rem',
+    height: 'auto',
+    width: '90%'
+  },
+
   //<div style={styleFooter.containerElementsfooterPart}>
-  //Very small, mobile, tablet
+  //Very small, mobile and tablets
+  //Portrait
   containerElementsfooterPart: {
     display: 'flex',
     justifyContent: 'space-evenly',
@@ -35,14 +75,28 @@ const styleFooter = {
   },
 
   //<div style={styleFooter.followTablet}>{data[0].follow}</div>
-  //Tablet
+  //Tablets
+  //Portrait
   follow: {
     fontSize: 'clamp(0.625rem, 0.4037rem + 1.2422vw, 1rem)',
     fontWeight: '500'
   },
 
+  //<div style={styleFooter.containerImagesFooterPartVerySmall}>
+  //Very small
+  //Portrait
+  containerImagesFooterPartVerySmall: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    height: 'auto',
+    width: '70%',
+    gap: '1rem'
+  },
+
   //<div style={styleFooter.containerImagesFooterPart}>
-  //Very small, mobile, tablet
+  //Mobile and tablets
+  //Portrait
   containerImagesFooterPart: {
     display: 'flex',
     justifyContent: 'space-evenly',
@@ -54,10 +108,22 @@ const styleFooter = {
 
   //<button style={styleFooter.buttonFooterPartVerySmall}/>
   //Very small
+  //Portrait
   buttonFooterPartVerySmall: {
     backgroundColor: 'transparent',
     height: 'auto',
-    width: '0.8rem',
+    width: '0.5rem',
+    border: 'none'
+  },
+
+  //<button style={styleFooter.buttonFooterTablet}/>
+  //Mobile and tablets
+  //Landscape
+  buttonFooterLand: {
+    backgroundColor: 'transparent',
+    height: 'auto',
+    width: '1rem',
+    fontSize: '0.938rem',
     border: 'none'
   },
 
