0f1b63c0cd0348db0f8a48d782d089658a28b454
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-10 00:40:24 +0200

    Create styleNews.js

---
+++ mern-app/src/styles/styleNews.js
@@ -0,0 +1,47 @@
+const styleNews = {
+  firstContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'purple',
+    height: '85vh',
+    width: '200vw'
+  },
+
+  leftContainer: {
+    backgroundColor: 'var(--black)',
+    height: '85vh',
+    width: '100vw'
+  },
+
+  rightContainer: {
+    backgroundColor: 'var(--dark-green)',
+    height: '85vh',
+    width: '100vw'
+  },
+
+  nav: {
+    backgroundColor: 'var(--gray)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '15vh',
+    width: '100vw',
+    position: 'fixed'
+  },
+
+  navLeftItem: {
+    fontSize: '1.5rem',
+    height: '100%',
+    width: '50%'
+  },
+
+  navRightItem: {
+    fontSize: '1.5rem',
+    height: '100%',
+    width: '50%'
+  }
+};
+
+export default styleNews;
