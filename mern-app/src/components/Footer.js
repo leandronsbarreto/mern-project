6e1ac89cee3c8d16e2c517971afa437190d35176
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-10 00:42:30 +0200

    Update Footer.js

--- mern-app/src/components/Footer.js
+++ mern-app/src/components/Footer.js
@@ -12,23 +12,31 @@ import {
 
 const Footer = () => {
   //Media queries
-  const isVerySmall = useMediaQuery({ query: '(max-width: 284px)' });
+  /*   const beforeVerySmal = useMediaQuery({ query: '(max-width: 190px)' }); */
+  const isVerySmall = useMediaQuery({
+    query: '(max-width: 284px)'
+  });
   const isMobile = useMediaQuery({
     query: '(min-width: 285px) and (max-width: 480px)'
   });
   const isTablet = useMediaQuery({
     query: '(min-width: 481px) and (max-width: 768px)'
   });
+  const isMobileAndTablet = useMediaQuery({
+    query: '(min-width: 285px) and (max-width: 768px)'
+  });
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
+  const isLandscape = useMediaQuery({ query: '(orientation: landscape)' });
 
   return (
     <>
-      {/* Up to 284px, Portrait and Landscape*/}
-      {isVerySmall && (
-        <footer style={styleFooter.containerFooter}>
+      {/*Very small, Landscape*/}
+      {isVerySmall && isLandscape && (
+        <footer style={styleFooter.containerFooterVerySmall}>
           <div style={styleFooter.footerPart}>
             <div style={styleFooter.containerElementsfooterPart}>
               <div style={styleFooter.follow}>{data.home.footer.follow}</div>
-              <div style={styleFooter.containerImagesFooterPart}>
+              <div style={styleFooter.containerImagesFooterPartVerySmall}>
                 <button
                   style={styleFooter.buttonFooterPartVerySmall}
                   className='buttonSvg'
@@ -80,8 +88,124 @@ const Footer = () => {
         </footer>
       )}
 
-      {/* Mobile devices, Portrait and Landscape*/}
-      {isMobile && (
+      {/*Very small, Portrait*/}
+      {isVerySmall && isPortrait && (
+        <footer style={styleFooter.containerFooterVerySmall}>
+          <div style={styleFooter.footerPart}>
+            <div style={styleFooter.containerElementsfooterPart}>
+              <div style={styleFooter.follow}>{data.home.footer.follow}</div>
+              <div style={styleFooter.containerImagesFooterPartVerySmall}>
+                <button
+                  style={styleFooter.buttonFooterPartVerySmall}
+                  className='buttonSvg'
+                  id='instagram'
+                  type='button'
+                  name='Go to Instagram website'
+                >
+                  <SlSocialInstagram alt={data.home.footer.altInstagram} />
+                </button>
+                <button
+                  style={styleFooter.buttonFooterPartVerySmall}
+                  className='buttonSvg'
+                  id='twitter'
+                  type='button'
+                  name='Go to Twitter website'
+                >
+                  <SlSocialTwitter alt={data.home.footer.altTwitter} />
+                </button>
+                <button
+                  style={styleFooter.buttonFooterPartVerySmall}
+                  className='buttonSvg'
+                  id='facebook'
+                  type='button'
+                  name='Go to Facebook website'
+                >
+                  <SlSocialFacebook alt={data.home.footer.altFacebook} />
+                </button>
+                <button
+                  style={styleFooter.buttonFooterPartVerySmall}
+                  className='buttonSvg'
+                  id='youtube'
+                  type='button'
+                  name='Go to YouTube website'
+                >
+                  <SlSocialYoutube alt={data.home.footer.altYoutube} />
+                </button>
+                <button
+                  style={styleFooter.buttonFooterPartVerySmall}
+                  className='buttonSvg'
+                  id='linkedin'
+                  type='button'
+                  name='Go to Linkedin website'
+                >
+                  <SlSocialLinkedin alt={data.home.footer.altLinkedin} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </footer>
+      )}
+
+      {/* Mobile devices, iPads, and tablets, Landscape*/}
+      {isMobileAndTablet && isLandscape && (
+        <footer style={styleFooter.containerFooterLand}>
+          <div style={styleFooter.footerPart}>
+            <div style={styleFooter.containerElementsfooterPartLand}>
+              <div style={styleFooter.follow}>{data.home.footer.follow}</div>
+              <div style={styleFooter.containerImagesFooterPart}>
+                <button
+                  style={styleFooter.buttonFooterLand}
+                  className='buttonSvg'
+                  id='instagram'
+                  type='button'
+                  name='Go to Instagram website'
+                >
+                  <SlSocialInstagram alt={data.home.footer.altInstagram} />
+                </button>
+                <button
+                  style={styleFooter.buttonFooterLand}
+                  className='buttonSvg'
+                  id='twitter'
+                  type='button'
+                  name='Go to Twitter website'
+                >
+                  <SlSocialTwitter alt={data.home.footer.altTwitter} />
+                </button>
+                <button
+                  style={styleFooter.buttonFooterLand}
+                  className='buttonSvg'
+                  id='facebook'
+                  type='button'
+                  name='Go to Facebook website'
+                >
+                  <SlSocialFacebook alt={data.home.footer.altFacebook} />
+                </button>
+                <button
+                  style={styleFooter.buttonFooterLand}
+                  className='buttonSvg'
+                  id='youtube'
+                  type='button'
+                  name='Go to YouTube website'
+                >
+                  <SlSocialYoutube alt={data.home.footer.altYoutube} />
+                </button>
+                <button
+                  style={styleFooter.buttonFooterLand}
+                  className='buttonSvg'
+                  id='linkedin'
+                  type='button'
+                  name='Go to Linkedin website'
+                >
+                  <SlSocialLinkedin alt={data.home.footer.altLinkedin} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </footer>
+      )}
+
+      {/* Mobile devices, Portrait*/}
+      {isMobile && isPortrait && (
         <footer style={styleFooter.containerFooter}>
           <div style={styleFooter.footerPart}>
             <div style={styleFooter.containerElementsfooterPart}>
@@ -138,8 +262,8 @@ const Footer = () => {
         </footer>
       )}
 
-      {/* iPads, tablets, Portrait and Landscape*/}
-      {isTablet && (
+      {/* iPads, tablets, Portrait*/}
+      {isTablet && isPortrait && (
         <footer style={styleFooter.containerFooter}>
           <div style={styleFooter.footerPart}>
             <div style={styleFooter.containerElementsfooterPart}>
