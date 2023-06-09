6bb1534528d45e44ce9f7e7e3edcf00b30af9e0f
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-10 00:40:36 +0200

    Create styleContentAbout.js

---
+++ mern-app/src/styles/styleContentAbout.js
@@ -0,0 +1,53 @@
+const styleContentAbout = {
+  mainContainer: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'column',
+    minHeight: '85vh',
+    width: '100%'
+  },
+
+  presentationContainer: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'column',
+    minHeight: '40vh',
+    width: '100%',
+    padding: '1rem'
+  },
+
+  aboutContainer: {
+    backgroundColor: 'yellow',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'column',
+    minHeight: '40vh',
+    width: '100%'
+  },
+
+  headerOne: {
+    padding: '0.7rem',
+    textAlign: 'justify',
+    fontSize: 'clamp(0.375rem, 0.1967rem + 2.1944vw, 1.25rem)',
+    fontWeight: '400'
+  },
+
+  imgContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    minHeight: '30vh',
+    width: '100%'
+  },
+
+  img: {
+    height: 'auto',
+    width: '70%',
+    padding: '0.7rem',
+    borderRadius: '20px'
+  }
+};
+
+export default styleContentAbout;
