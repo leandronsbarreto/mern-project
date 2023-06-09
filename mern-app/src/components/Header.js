270fbc4927de221c7208956bde1f85ee76680e6e
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-10 00:42:32 +0200

    Update Header.js

--- mern-app/src/components/Header.js
+++ mern-app/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   //Media queries
   /*   const beforeVerySmal = useMediaQuery({ query: '(max-width: 190px)' }); */
   const isVerySmall = useMediaQuery({
-    query: '(min-width: 191px) and (max-width: 284px)'
+    query: '(max-width: 284px)'
   });
   const isMobile = useMediaQuery({
     query: '(min-width: 285px) and (max-width: 480px)'
@@ -16,17 +16,42 @@ const Header = () => {
   const isTablet = useMediaQuery({
     query: '(min-width: 481px) and (max-width: 768px)'
   });
+  const isMobileAndTablet = useMediaQuery({
+    query: '(min-width: 285px) and (max-width: 768px)'
+  });
   const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
+  const isLandscape = useMediaQuery({ query: '(orientation: landscape)' });
 
   return (
     <>
+      {/*Very small, landscape*/}
+      {isVerySmall && isLandscape && (
+        <nav style={styleHeader.containerHeader}>
+          <div style={styleHeader.insideContainerHeaderVerySmall}>
+            <h1 style={styleHeader.containerLogoHeaderLandVs}>
+              Help Refugees Alliance
+            </h1>
+            <button
+              id='login'
+              type='button'
+              name='Login button'
+              className='buttonSvg'
+              style={styleHeader.buttonHeaderVerySmallLand}
+            >
+              <SlLogin alt={data.home.header.altLogin} />
+              Login
+            </button>
+          </div>
+        </nav>
+      )}
+
       {/*Very small, portrait*/}
       {isVerySmall && isPortrait && (
         <nav style={styleHeader.containerHeader}>
           <div style={styleHeader.insideContainerHeaderVerySmall}>
-            <div style={styleHeader.containerLogoHeader}>
+            <h1 style={styleHeader.containerLogoHeader}>
               Help Refugees Alliance
-            </div>
+            </h1>
             <button
               id='login'
               type='button'
@@ -38,19 +63,33 @@ const Header = () => {
               Login
             </button>
           </div>
-          {/* <div style={styleHeader.insideContainerHeaderVerySmall}>
-            <div style={styleHeader.containerLogoHeader}></div>
+        </nav>
+      )}
+
+      {/*Mobile devices, iPads, and tablets, landscape*/}
+      {isMobileAndTablet && isLandscape && (
+        <nav style={styleHeader.containerHeaderLand}>
+          <div style={styleHeader.insideContainerHeader}>
+            <div style={styleHeader.containerImgHeaderLand}>
+              <img
+                style={styleHeader.imgHeaderLand}
+                src={process.env.PUBLIC_URL + '/images/logo.png'}
+                alt={data.home.header.altLogo}
+                title={data.home.header.titleLogo}
+              />
+              <h1>Help Refugees Alliance</h1>
+            </div>
             <button
               id='login'
               type='button'
               name='Login button'
               className='buttonSvg'
-              style={styleHeader.buttonHeader}
+              style={styleHeader.buttonHeaderLand}
             >
               <SlLogin alt={data.home.header.altLogin} />
               Login
             </button>
-          </div> */}
+          </div>
         </nav>
       )}
 
@@ -61,10 +100,13 @@ const Header = () => {
             <div style={styleHeader.containerImgHeader}>
               <img
                 style={styleHeader.imgHeader}
-                src={process.env.PUBLIC_URL + '/images/logo-black.png'}
+                src={process.env.PUBLIC_URL + '/images/logo.png'}
                 alt={data.home.header.altLogo}
                 title={data.home.header.titleLogo}
               />
+              <h1>
+                Help <br /> Refugees <br /> Alliance
+              </h1>
             </div>
             <button
               id='login'
@@ -87,10 +129,13 @@ const Header = () => {
             <div style={styleHeader.containerImgHeader}>
               <img
                 style={styleHeader.imgHeader}
-                src={process.env.PUBLIC_URL + '/images/logo-black.png'}
+                src={process.env.PUBLIC_URL + '/images/logo.png'}
                 alt={data.home.header.altLogo}
                 title={data.home.header.titleLogo}
               />
+              <h1>
+                Help <br /> Refugees <br /> Alliance
+              </h1>
             </div>
             <button
               id='login'
