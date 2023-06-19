0bd4d74c0a26b1044d3333e5525a922d0349633a
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-19 18:14:44 +0200

    Create about.js

---
+++ mern-app/src/pages/about.js
@@ -0,0 +1,13 @@
+// import { useEffect } from 'react';
+
+const About = () => {
+  /*   useEffect(() => {
+    const pathAbout = window.location.pathname;
+    pathAbout === '/aboutandmission/about'
+      ? (document.querySelector('.leandro').style.display = 'none')
+      : console.log('Read about us!');
+  }, []); */
+  return <h1>This is about</h1>;
+};
+
+export default About;
