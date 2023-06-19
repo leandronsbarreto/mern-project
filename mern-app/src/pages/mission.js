b1c80ca5a9e6776dffec7f06427be0d8ead7048d
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-19 18:14:40 +0200

    Create mission.js

---
+++ mern-app/src/pages/mission.js
@@ -0,0 +1,13 @@
+// import { useEffect } from 'react';
+
+const Mission = () => {
+  /*   useEffect(() => {
+    const pathMission = window.location.pathname;
+    pathMission === '/aboutandmission/mission'
+      ? (document.querySelector('.leandro').style.display = 'none')
+      : console.log('Get to know our mission');
+  }, []); */
+  return <h1>This is Mission</h1>;
+};
+
+export default Mission;
