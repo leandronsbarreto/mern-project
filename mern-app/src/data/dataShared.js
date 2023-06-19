93f061cbb6f0a8dc3eb473c73614930a4eb962e6
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-19 18:14:49 +0200

    Create dataShared.js

---
+++ mern-app/src/data/dataShared.js
@@ -0,0 +1,30 @@
+const dataShared = [
+  {
+    presentationContent: [
+      'Help Refugees Alliance is a digital platform that counts with a database with information about volunteers worldwide willing to offer refugees a new home.',
+      'It has also ranked as one of the leading international non-governmental organisations aimed at giving assistance to refugees.',
+      'Our database is connected with those of other organisations such as Amnesty International and Refugees Foundation. Cooperation is the key for our success.'
+    ],
+    alt: 'Refugees fleeing the war in Ukraine are crossing the border into Poland at the Medyka border. Over 2 million people have fled into Poland one month into the conflict.',
+    title:
+      'By Ingebjørg Kårstad/NRC, Greece, 15th March 2022. Source: Norwegian Refugee Council.',
+    infoBox: [
+      'According to the United Nations High Commissioner for Refugees (UNHCR), at mid-2022 103 million people were forcibly displaced worldwide - 32.5 million are refugees.',
+      'UNHCR also estimates that between 2018 and 2021 an average of between 350,000 and 400,000 children were born into a refugee life per year.'
+    ]
+  },
+  {
+    newsletterContent: 'Sign up our weekly newsletter.',
+    alt: 'A crowd of refugees in the reception centre of the Hamam al-Alil camp for people displaced from Mosul, Iraq.',
+    title:
+      'By Wolfgang Gressman/NRC, Iraq, 16th March 2017. Source: Norwegian Refugee Council.'
+  }
+  /*   aboutContent: {
+
+  },
+  missionContent: {
+
+  } */
+];
+
+export default dataShared;
