9b0c4ab0c4312c06d3f579a18fd5366f89e4f64e
Leandro Barreto <leandrobarreto@gmx.de>
2023-06-19 18:14:52 +0200

    Create dataLayoutNewsPress.js

---
+++ mern-app/src/data/dataLayoutNewsPress.js
@@ -0,0 +1,6 @@
+const allTabs = [
+  { id: 'news', icon: '📱', label: 'Articles', content: 'leandro' },
+  { id: 'press', icon: '📨', label: 'Press request', content: 'leandro' }
+];
+
+export default allTabs;
